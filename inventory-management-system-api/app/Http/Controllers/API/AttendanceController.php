14bfb51988490ea812084b636782272db7f3499e
joynalam67598 <59138477+joynalam67598@users.noreply.github.com>
2021-10-12 01:46:18 +0600

    complete attendance functionalities

--- inventory-management-system-api/app/Http/Controllers/API/AttendanceController.php
+++ inventory-management-system-api/app/Http/Controllers/API/AttendanceController.php
@@ -34,7 +34,7 @@ class AttendanceController extends Controller
                 $attendance->att_year = date("Y");
                 $attendance->save();
             }
-            
+
             return response()->json([
                 "message"=>"Attendance taken sucessfully!","status"=>200
             ]);
@@ -42,24 +42,38 @@ class AttendanceController extends Controller
     }
     public function getAttendances(){
         $attendances = DB::table('attendances')
-            ->select('edit_date')->groupBy('edit_date')->get();
+            ->select('att_date')->groupBy('att_date')->get();
         return response()->json([
             'attendances'=>$attendances,"status"=>200
         ]);
     }
 
     public function getAttendance($date){
-        $attendance = DB::table('attendances')
+        $employees = DB::table('attendances')
             ->join('employees','employees.id','=','attendances.employee_id')
             ->select('attendances.*','employees.name','employees.photo')
             ->where('attendances.edit_date','=',$date)
             ->get();
         return response()->json([
-            'attendance'=>$attendance,"status"=>200
+            'employees'=>$employees,"status"=>200
         ]);
     }
 
-    public function updateAttendance(){
-        //
+    public function updateAttendance(Request $request){
+
+        for($i=0;$i<count($request->all());$i++)
+            {
+                $attendance = Attendance::where('att_date','=',$request[$i][2])
+                                          ->where('employee_id','=',$request[$i][0])
+                                          ->first();
+                $attendance->attendance =  $request[$i][1];
+                $attendance->edit_date = date('d-m-Y');
+                $attendance->update();
+            }
+
+            return response()->json([
+                "message"=>"Attendance taken sucessfully!","status"=>200
+            ]);
+        
     }
 }
---
+++ inventory-management-system-app/src/app/views/attendance/EditAttendance.js
@@ -0,0 +1,242 @@
+import {
+    Button,
+    Card,
+    CardContent,
+    CardHeader,
+    FormControlLabel,
+    Grid,
+    Radio,
+    RadioGroup,
+    Table,
+    TableBody,
+    TableCell,
+    TableRow,
+} from '@material-ui/core'
+import Axios from 'axios'
+import _ from 'lodash'
+import { useEffect, useReducer, useState } from 'react'
+import { useHistory } from 'react-router'
+import { Breadcrumb } from '../../components'
+
+const initialState = null
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'create':
+            let attendanceDataObject = {}
+            action.value.map((employee) => {
+                attendanceDataObject[employee.employee_id] = {
+                    id: employee.employee_id,
+                    attendance: employee.attendance,
+                }
+            })
+            return attendanceDataObject
+        case 'assign':
+            const newDataObject = _.cloneDeep(action.obj)
+            newDataObject[action.index].attendance = action.value
+            return newDataObject
+        default:
+            return state
+    }
+}
+
+export default function EditAttendance() {
+    const history = useHistory()
+    const { location } = history
+    const { state } = location
+    const { attendanceDate } = state
+    const [employeesAttendance, dispatch] = useReducer(reducer, initialState)
+    const [employees, setEmployees] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [errors, setErrors] = useState({})
+    console.log(employees)
+
+    useEffect(() => {
+        async function fetchEmployees() {
+            try {
+                setLoading(true)
+                const res = await Axios.get(
+                    `http://localhost:8000/api/attendance/edit/${attendanceDate}`
+                )
+                if (res.data.status === 200) {
+                    setEmployees(res.data.employees)
+                    dispatch({
+                        type: 'create',
+                        value: res.data.employees,
+                    })
+                    setLoading(false)
+                }
+            } catch (err) {
+                console.log(err.response.data.errors)
+                setLoading(false)
+            }
+        }
+        fetchEmployees()
+    }, [])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        dispatch({
+            type: 'assign',
+            index: name,
+            obj: employeesAttendance,
+            value: value,
+        })
+    }
+
+    const updateAttendance = async (e) => {
+        e.preventDefault()
+        const data = []
+        Object.keys(employeesAttendance).forEach((key) =>
+            data.push([
+                employeesAttendance[key].id,
+                employeesAttendance[key].attendance,
+                attendanceDate,
+            ])
+        )
+        try {
+            setLoading(true)
+            console.log(employeesAttendance)
+            console.log(data)
+            const res = await Axios.post(
+                'http://localhost:8000/api/attendance/update',
+                data
+            )
+            console.log(res)
+            if (res.data.status === 200) {
+                history.push({
+                    pathname: '/attendance/manage',
+                })
+            }
+        } catch (err) {
+            setErrors({ ...err.response.data.errors })
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className="m-sm-30">
+            <div className="mb-sm-30">
+                <Breadcrumb
+                    routeSegments={[
+                        { name: 'Attendance', path: '/attendance/take' },
+                        { name: 'Edit Attndance' },
+                    ]}
+                />
+            </div>
+            <Card
+                style={{
+                    backgroundColor: '#212f52',
+                    maxWidth: '750px',
+                    margin: '0 auto',
+                    padding: '0 5px 5px',
+                    border: '5px solid',
+                }}
+            >
+                <CardHeader
+                    title="Edit Attendance"
+                    style={{
+                        borderRadius: '10px',
+                        textAlign: 'center',
+                        color: 'white',
+                    }}
+                />
+
+                <CardContent
+                    style={{
+                        backgroundColor: '#fcfdff',
+                        borderRadius: '10px',
+                        textAlign: 'center',
+                    }}
+                >
+                    <form onSubmit={updateAttendance}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Table
+                                    style={{
+                                        border: '1px solid',
+                                        tableLayout: 'auto',
+                                    }}
+                                >
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell>Employee Name</TableCell>
+                                            <TableCell>Status</TableCell>
+                                        </TableRow>
+                                        {!loading &&
+                                            employees.map((employee) => (
+                                                <TableRow>
+                                                    <TableCell>
+                                                        {employee.name}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <RadioGroup
+                                                            name={
+                                                                employee.employee_id
+                                                            }
+                                                            defaultValue={
+                                                                employeesAttendance[
+                                                                    employee
+                                                                        .employee_id
+                                                                ].attendance
+                                                            }
+                                                            onChange={
+                                                                handleChange
+                                                            }
+                                                            row
+                                                            key={employee.id}
+                                                        >
+                                                            <FormControlLabel
+                                                                value="Present"
+                                                                control={
+                                                                    <Radio
+                                                                        required
+                                                                    />
+                                                                }
+                                                                label="Present"
+                                                                style={{
+                                                                    color: 'green',
+                                                                }}
+                                                            />
+                                                            <FormControlLabel
+                                                                style={{
+                                                                    color: 'red',
+                                                                }}
+                                                                value="Absent"
+                                                                control={
+                                                                    <Radio
+                                                                        required
+                                                                    />
+                                                                }
+                                                                label="Absent"
+                                                            />
+                                                        </RadioGroup>
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                    </TableBody>
+                                </Table>
+                            </Grid>
+                            <Grid item xs={12} className="text-right">
+                                <Button
+                                    disabled={loading}
+                                    type="submit"
+                                    variant="contained"
+                                    color="secondary"
+                                    fullWidth
+                                    style={{
+                                        fontSize: '1.1rem',
+                                        marginTop: '2px',
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    Update
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
